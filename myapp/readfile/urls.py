2952bf1bc7499a1c8a0f49c86e4d6e4e4b68d22e
Nandu Chandran <nanduchandran121@gmail.com>
2022-07-26 17:30:20 +0530

    changed card style

--- myapp/readfile/urls.py
+++ myapp/readfile/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='blog-home'),
+    path('', views.index, name='blog_home'),
+    path('about/', views.about, name='blog-about'),
 ]
