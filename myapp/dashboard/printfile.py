d04bc5b770d7acca7bf6b2f8058f45d0c007ae83
Nandu Chandran <nanduchandran121@gmail.com>
2022-08-10 14:34:38 +0530

    read full text file

--- myapp/dashboard/printfile.py
+++ myapp/dashboard/printfile.py
@@ -3,15 +3,20 @@ import argparse
 
 def printfile(filename,printfull):
  print("\n>>>>>>filename from printfile function\n",filename)
- with open(filename, "r") as f:
+ with open(filename, "r") as file:
     try:
-      file_content = [l for l in (line.strip() for line in f) if l]
       if printfull == True:
-        return file_content
+         file_content= fulldisplay(file)
+         return file_content
       else:
-        return(random.choice(file_content))
+         file_content = [l for l in (line.strip() for line in file) if l]
+         return(random.choice(file_content))
     except:
       return("Empty file")
+
+def fulldisplay(file_obj):
+  content= file_obj.read()
+  return content
 
 
 
